3eedcd59e75be3a51d0c0da67ecd55e2faa9e836
rembosatyam7 <135469132+rembosatyam7@users.noreply.github.com>
2023-06-04 14:53:47 +0530

    my project

---
+++ includes/secondfooter.php
@@ -0,0 +1,37 @@
+<footer class="page-footer">
+    <div class="container footer-info">
+        <div class="row">
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow">Developers</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="#"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i>KP</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="#"><i class="tiny material-icons" style="position:relative;top:3px">developer_mode</i>KP</a></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow">Categories</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category.php">SmartPhone</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category.php">SmartCam</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category.php">SmartLaptop</a></li>
+                </ul>
+            </div>
+
+            <div class="col s12 l4 ">
+                <h5 class="grey-text text-lighten-3 animated slideInUp wow"></h5></br>
+                <ul>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category.php">SmartWatch</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category.php">SmartSpeaker</a></li>
+                    <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category.php">SmartVR</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © <?= Date('Y'); ?> All rights reserved
+            <a class="grey-text text-lighten-4 right animated slideInUp wow" href="#"><i class="fa fa-heartbeat animated pulse infinite red-text"></i>KP Store</a>
+        </div>
+    </div>
+</footer>
